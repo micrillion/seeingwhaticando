54f3b328faff8c503f38d036f96efbca692bdcc6
micrillion <tiffanylward76@gmail.com>
2025-11-28 18:54:11 -0500

    slight nerf to ID1 but buffed other IDs

--- src/core/dimensions/infinity-dimension.js
+++ src/core/dimensions/infinity-dimension.js
@@ -40,21 +40,21 @@ class InfinityDimensionState extends DimensionState {
     super(() => player.dimensions.infinity, tier);
     const UNLOCK_REQUIREMENTS = [
       undefined,
-      DC.E1100,
-      DC.E1900,
-      DC.E2400,
-      DC.E10500,
-      DC.E30000,
-      DC.E45000,
+      DC.E1000,
+      DC.E1500,
+      DC.E2000,
+      DC.E7500,
+      DC.E25000,
+      DC.E40000,
+      DC.E48000,
       DC.E54000,
-      DC.E60000,
     ];
     this._unlockRequirement = UNLOCK_REQUIREMENTS[tier];
-    const COST_MULTS = [null, 1e3, 1e6, 1e8, 1e10, 1e15, 1e20, 1e25, 1e30];
+    const COST_MULTS = [null, 1e3, 1e6, 1e9, 1e12, 1e14, 1e23, 1e27, 1e32];
     this._costMultiplier = COST_MULTS[tier];
-    const POWER_MULTS = [null, 50, 30, 10, 5, 5, 5, 5, 5];
+    const POWER_MULTS = [null, 45, 30, 20, 15, 10, 7, 6, 5];
     this._powerMultiplier = POWER_MULTS[tier];
-    const BASE_COSTS = [null, 1e8, 1e9, 1e10, 1e20, 1e140, 1e200, 1e250, 1e280];
+    const BASE_COSTS = [null, 1e7, 1e8, 1e19, 1e18, 1e150, 1e220, 1e270, 1e300];
     this._baseCost = new Decimal(BASE_COSTS[tier]);
     this.ipRequirement = BASE_COSTS[1];
   }
